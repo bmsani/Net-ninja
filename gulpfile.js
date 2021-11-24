1dd181a5a92da9813badb427b24021cf8208a529
bmsani <bmsani6@gmail.com>
2021-11-24 13:04:34 +0600

    20 complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
   return src('sani/**/*.scss')
     .pipe(sass())
+    .pipe(purgecss({ content:['*.html']}))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['sani/**/*.scss'], buildStyles)
+  watch(['sani/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
